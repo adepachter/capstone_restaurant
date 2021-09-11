fa014046410960d7596aaa36c94ad984f02148fd
Arno De Pachter <arno@zeelandmultimedia.nl>
2021-09-11 18:23:02 +0200

    test

---
+++ frontend/components/AppContext.js
@@ -0,0 +1,18 @@
+/* /context/AppContext.js */
+
+import React from "react";
+// create auth context with default value
+
+// set backup default for isAuthenticated if none is provided in Provider
+const AppContext = React.createContext(
+    {isAuthenticated:false, 
+        cart: {items:[], 
+        total:0},
+        addItem:()=>{},
+        removeItem:()=>{},
+        user:false, 
+        test: "hallotest",
+        setTest:()=>{},
+        setUser:()=>{}
+    });
+export default AppContext;
